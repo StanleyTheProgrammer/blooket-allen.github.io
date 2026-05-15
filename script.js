271a3cf6bf7c13da07ea9de07821e62db93c12f0
StanleyTheProgrammer <p2115108@qbps.edu.hk>
2026-05-15 12:25:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+(()=>{
 function getStateNode() {
     return Object.values(document.querySelector("#app>div>div")).find(e=>e.children).children.find(m=>m._owner&&m._owner.stateNode)._owner.stateNode;
 }
@@ -8,14 +8,11 @@ function bypass(){
     document.body.appendChild(_bypass);
     return {window: _bypass.contentWindow, document: _bypass.contentDocument}
 }
+window.setInterval(()=>{
+    try{
+        getStateNode().state.question.correctAnswers = getStateNode().state.question.answers
+    }catch {
 
-    let pname = bypass().window.prompt("Player Name");
-    let gold = bypass().window.prompt("Gold");
-    getStateNode().props.liveGameController.setVal({
-        path: "c/" + getStateNode().props.client.name,
-        val: {
-            b: getStateNode().props.client.blook,
-            g: getStateNode().state.gold,
-            tat: pname + ":swap:" + gold
     }
-
+});
+})();
